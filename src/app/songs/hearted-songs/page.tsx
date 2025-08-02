585ee0370aad19d81a0a38b900ff1f2156220271
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2025-08-02 16:48:43 +0700

    fix build

--- src/app/songs/hearted-songs/page.tsx
+++ src/app/songs/hearted-songs/page.tsx
@@ -1,65 +1,19 @@
 "use server";
-import { songForList } from "@/lib/prisma-includes";
-import {
-  createBatchAccessLinksImages,
-  transformSongDataWithUrls,
-} from "@/lib/song-utils";
-import { prisma } from "@/utils/prisma";
+import { transformSongDataWithUrls } from "@/lib/song-utils";
 import { SignIn } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-import { unstable_cache } from "next/cache";
 import React from "react";
 import MainPage from "../all/main-page";
+import { getCachedHeartedSongs } from "@/lib/cache/songs-cache";
 
-export const getCachedHeartedSongs = unstable_cache(
-  async (clerkId: string, imageExpires: number = 3600) => {
-    const userDb = await prisma.users.findUnique({
-      where: { clerkId },
-      include: {
-        HeartedSongs: {
-          include: {
-            Songs: {
-              include: songForList,
-            },
-          },
-          orderBy: {
-            createdAt: "desc",
-          },
-          take: 10, // Lấy 20 bài để có đủ cho next/prev
-        },
-      },
-    });
-
-    if (!userDb || !userDb.HeartedSongs.length) {
-      return { heartedSongs: [], imageUrls: [], hasMore: false };
-    }
-
-    const songs = userDb.HeartedSongs.map((item) => item.Songs);
-
-    // Tạo image URLs
-    const { imageUrls } = await createBatchAccessLinksImages(
-      songs,
-      imageExpires
-    );
-
-    return {
-      heartedSongs: songs,
-      imageUrls,
-      hasMore: userDb.HeartedSongs.length === 10,
-    };
-  },
-  ["hearted-songs"],
-  {
-    revalidate: 1800, // 30 phút
-    tags: ["songs", "hearted-songs"],
-  }
-);
 const Page = async () => {
   const user = await currentUser();
   if (!user) {
     return <SignIn />;
   }
-  const { heartedSongs, imageUrls, hasMore } = await getCachedHeartedSongs(user.id);
+  const { heartedSongs, imageUrls, hasMore } = await getCachedHeartedSongs(
+    user.id
+  );
 
   const songData = transformSongDataWithUrls(heartedSongs, imageUrls);
 
